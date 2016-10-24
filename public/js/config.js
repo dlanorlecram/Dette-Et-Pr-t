b49f8917ac227a9730b31b1687a4b1e545549d15
dlanorlecram <ronald.marcel.mr@gmail.com>
2016-10-24 17:11:52 +0200

    fin de journée

--- public/js/config.js
+++ public/js/config.js
@@ -1,9 +1,9 @@
-console.log('chargement de config.js');
+//console.log('chargement de config.js');
 app.config(function($routeProvider){
   $routeProvider
     .when('/', {templateUrl: 'credit.html', controller: 'creditCtrl'})
-    .when('/credit', {templateUrl: 'credit.html', controller: 'creditCtrl'})
-    .when('/payback', {templateUrl: 'payback.html', controller: 'paybackCtrl'})
+    .when('/credit', {templateUrl: 'credit.html', controller: 'creditCtrl','creditCtrl'})
+    .when('/payback', {templateUrl: 'payback.html', controller: 'creditCtrl','paybackCtrl'})
     //.when('/totals', {templateUrl: 'payback.html', controller: 'paybackCtrl'})
     .otherwise({redirectTo:'/'});
 });
--- public/js/myapp.js
+++ public/js/myapp.js
@@ -1,33 +1,8 @@
 //console.log('chargement de Myapp.js');
-var app = angular.module('myApp', ['ngRoute']);
-//création d'un service pour lier des donnée entre deux controller
-//Mise en commun json files
-/*app.factory('dealFactory', function($http){
-    //var data=[];
-    var factory = {
-
-        getDepenses : function(){
-            $http.get('db/depenses.json').then(function(answer){
-            request= answer.data.records;
-            console.log(request);
-                return request;
-            });
-        },
-        getsomething: function(){
-            return false;
-        },
-        getMoney: function(){
-            return factory.getDepenses;
-            console.log(factory.depenses);
-        }
-    }
-    return factory;
-});*/
-
-
+var app = angular.module('myApp', []);
 
 // Controller to manage donatation
-app.controller('creditCtrl', function($scope, $http, $route){
+app.controller('creditCtrl', function($scope, $http){
     $http.get('db/depenses.json').then(function(reponse){
         $scope.money = reponse.data.records;
         //$scope.money = dealFactory.getDepenses();
@@ -35,65 +10,15 @@ app.controller('creditCtrl', function($scope, $http, $route){
     });
 });
 
-
 //Controller to manage payback
-app.controller('paybackCtrl', function($scope, $http, $route){
+app.controller('paybackCtrl', function($scope, $http){
      $http.get('db/users.json').then(function(reponse){
         $scope.users = reponse.data.records;
-        var id_test;
-         for(var i=0; i < $scope.users.id.length; i++){
-         tab.push($scope.users[i]);
-         console.log(tab);
-         }
+        $scope.aUsers = [];
+        //Generating array aUsers with structure [id => username]
+        for($user in $scope.users){
+          $scope.aUsers[$scope.users[$user].Id] = $scope.users[$user].username;
+        }
+        console.log($scope.aUser);
     });
 });
-
-
-
-
-
-/*
-
-app.controller('depensesCtrl', function ($scope, $http) {
-    $http.get("Data/depenses.json").then(function (response) {
-        $scope.myData = response.data.records;
-    });
-});
-
-*/
-
-
-
-/*app.factory('postFactory',function(){
-  var factory = {
-      users: false,
-    user= $http.get('/db')  ,
-
-    getUsers: function(){
-      return factory.posts;
-    },
-    //ma fonction pour parcourir un user
-    getUser: function(id){
-      var user = {};
-      angular.forEach(factory.users, function(value, key){
-        if(value.id == id){
-            console.log('value id' + value)
-            post = value.id;
-            console.log('user'+ user);
-          }
-      });
-      return post;
-    },
-  }
-  return factory;
-})
-
-// controller to manage posts
-app.controller('postsCtrl', function($scope, postFactory){
-  $scope.posts = postFactory.getPosts();
-});
-// controller to manage comments
-app.controller('commentsCtrl', function($scope, postFactory, $routeParams){
-  console.log($routeParams);
-  $scope.comments = postFactory.getPost().comments;
-});*/
