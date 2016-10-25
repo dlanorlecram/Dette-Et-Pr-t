60366534b27e138561982b71182abec3fa374431
dlanorlecram <ronald.marcel.mr@gmail.com>
2016-10-25 17:33:34 +0200

    fin de journé

--- public/js/myapp.js
+++ public/js/myapp.js
@@ -2,23 +2,55 @@
 var app = angular.module('myApp', []);
 
 // Controller to manage donatation
-app.controller('creditCtrl', function($scope, $http){
+app.controller('creditCtrl', function($scope, $http, $rootScope){
     $http.get('db/depenses.json').then(function(reponse){
-        $scope.money = reponse.data.records;
+        $scope.depenses = reponse.data.records;
         //$scope.money = dealFactory.getDepenses();
+        //Variable qui permet de créer le premier tableau à explorer.
+                $scope.aConcernes = [];
+                //Tableau qui stock les nom un par un et qui est ensuite join
+                $scope.aConcernesElement = [];
+                //Tableau avec la liste des noms mais le mauvais index
+                $scope.aConcernesTrue = [];
+                //Le bon tableau à utiliser
+                $scope.aConcernesFinal = [];
+
+                for($depense in $scope.depenses) {
+                    //On crée le tableau à explorer (le split est important car il permet de rendre ce tableau exploitable pour la boucle suivante)
+                    $scope.aConcernes[$depense] = $scope.depenses[$depense].Concernes.split(',');
+                }
+
+                //le $scope.aConcernes est un tableau qui contient des tableaux (2 niveaux, donc 2 boucles for)
+                for ($i=0; $i < $scope.aConcernes.length; $i++) {
+                    for ($x=0; $x < $scope.aConcernes[$i].length; $x++) {
+                        //On transforme les ID en nom.
+                        $scope.aConcernesElement.push($rootScope.aUsers[$scope.aConcernes[$i][$x]]);
+                    }
+                    //On join les nom pour obtenir une ligne qu'on assigne dans un tableau unique
+                    $scope.aConcernesElement = $scope.aConcernesElement.join(", ");
+                    $scope.aConcernesTrue.push($scope.aConcernesElement);
+                    //On réinitialise la ligne pour faire une nouvelle
+                    $scope.aConcernesElement = [];
+                }
+
+                //Cette boucle est pour créer un tableau qui a pour clés les ID des dépenses et pour valeur les lignes créer plus tôt.
+                for($depense in $scope.depenses) {
+                    $scope.aConcernesFinal[$scope.depenses[$depense].Id] = $scope.aConcernesTrue[$depense];
+                }
 
     });
 });
 
 //Controller to manage payback
-app.controller('paybackCtrl', function($scope, $http){
+app.controller('paybackCtrl', function($scope, $http, $rootScope){
      $http.get('db/users.json').then(function(reponse){
         $scope.users = reponse.data.records;
-        $scope.aUsers = [];
+        $rootScope.aUsers = [];
         //Generating array aUsers with structure [id => username]
         for($user in $scope.users){
-          $scope.aUsers[$scope.users[$user].Id] = $scope.users[$user].username;
+          $rootScope.aUsers[$scope.users[$user].Id] = $scope.users[$user].username;
         }
         console.log($scope.aUser);
+
     });
 });
---
+++ public/js/script.js
@@ -0,0 +1,6 @@
+$( document ).ready(function(){
+    console.log( "ready!" );
+    $('.toggle_menu').click(function(){
+      $('.menu_hide').hide();
+    });
+});
